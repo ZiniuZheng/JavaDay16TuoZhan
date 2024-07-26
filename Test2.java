4366cafaa7fbed542d7b88fcd7297ba7058402c7
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-07-26 11:57:51 +0800

    Create Test2.java

---
+++ Test2.java
@@ -0,0 +1,28 @@
+package tuozhan;
+
+import java.text.SimpleDateFormat;
+public class Test2 {
+
+	public static void main(String[] args) {
+		String s = "2017年02月02日";
+		SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日");
+		
+		try {
+			int[] arr = new int[2];
+			int i = arr[2];
+		}catch(Exception e) {
+			//在这里弹出一个提示框
+			e.printStackTrace();
+		}finally {
+			
+		}
+		
+		try {
+			
+		}finally {
+			
+		}
+		
+		//format.parse(s);
+	}
+}
